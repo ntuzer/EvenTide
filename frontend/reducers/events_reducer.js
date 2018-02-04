c0cc14732627db872a12f69f8b6185f9beaa4dd1
Rewel Garcia <rewelgarcia@Rewels-MacBook-Air.local>
2018-02-04 14:05:26 -0800

    Create events reducer

---
+++ frontend/reducers/events_reducer.js
@@ -0,0 +1,27 @@
+import merge from 'lodash/merge';
+
+import {
+  RECEIVE_EVENTS,
+  RECEIVE_EVENT,
+  RECEIVE_MY_EVENTS,
+} from '../actions/event_actions';
+
+const eventsReducer = (preloadedState = {}, action) => {
+  Object.freeze(preloadedState);
+  let newState;
+  switch (action.type) {
+    case RECEIVE_EVENTS:
+      newState = merge({}, action.albums);
+      return newState;
+    case RECEIVE_EVENT:
+      newState = merge(
+        {},
+        preloadedState,
+        {[action.albums.id]: action.albums}
+      );
+      return newState;
+    // case RECEIVE_MY_EVENTS:
+    default:
+      return preloadedState;
+  }
+};
