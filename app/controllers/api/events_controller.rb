c0ada0d3b237cf20c6cb4c88dafb5d6c4fe701fb
Rewel Garcia <rewelgarcia@Rewels-MacBook-Air.local>
2018-02-03 21:37:30 -0800

    Write Events controller

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,25 +1,44 @@
 class Api::EventsController < ApplicationController
-  def create
-    @Event = Event.new(event_params)
+  before_action :require_logged_in, only: [:create, :index, :destroy]
 
+  def create
+    @event = Event.new(event_params)
+    @event.organizer_id = current_user.id
+    if @event.save
+      render :show
+    else
+      render json: @event.errors.full_messages, status: 422
+    end
   end
 
   def show
-
+    @event = Event.find_by(id: params[:id])
+    if @event
+      render :show
+    else
+      render json: ['Event not found!'], status: 404
+    end
   end
 
   def index
-
+    @events = Event.all
   end
 
   def destroy
-
+    @event = Event.find_by(id: params[:id])
+    if @event && current_user.id == @event.organizer_id
+      @event.delete
+      render :show
+    else
+      render json: ["Unauthorized"], status: 404
+    end
   end
 
   private
 
   def event_params
-    params.require(:event).permit()
+    params.require(:event).permit(:title, :location, :description,
+      :event_image_url, :category_id, :min_price, :max_price, :start_date, :end_date)
   end
 
 end
