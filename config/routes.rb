cb5340a342a036851bad25703490829997075af5
Rewel Garcia <rewelgarcia@Rewels-MacBook-Air.local>
2018-02-04 11:50:27 -0800

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: :json} do
-    resources :benches, only: [:index, :show, :create]
-    resources :reviews, only: [:create]
-    resources :users, only: [:create]
-    resource :session, only: [:create, :destroy, :show]
-    resources :favorites, only: [:create, :destroy]
+    resources :users, only: [:create, :show, :update] do
+      resources :events, only: [:index]
+    end
+    resource :session, only: [:create, :destroy]
+    resources :events, only: [:index, :show, :create, :update, :destroy]
   end
 
   root "static_pages#root"
---
+++ frontend/util/events_util.js
@@ -0,0 +1,27 @@
+export const fetchEvents = () => {
+  return $.ajax({
+    url: '',
+    method: '',
+  });
+};
+export const fetchEvent = (id) => {
+  return $.ajax({
+    url: '',
+    method: '',
+  });
+};
+
+export const createEvent = event => {
+  return $.ajax({
+    url: '',
+    method: '',
+    data: { event }
+  });
+};
+
+export const deleteEvent = eventId => {
+  return $.ajax({
+    url: '',
+    method: '',
+  });
+};
