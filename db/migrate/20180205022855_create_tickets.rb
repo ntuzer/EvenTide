0e8d88d9aa5437fa42c85c56325cdba51afb18e0
Rewel Garcia <rewelgarcia@Rewels-MacBook-Air.local>
2018-02-04 19:38:43 -0800

    Create categories controller

--- db/migrate/20180205022855_create_tickets.rb
+++ db/migrate/20180205022855_create_tickets.rb
@@ -2,9 +2,10 @@ class CreateTickets < ActiveRecord::Migration[5.1]
   def change
     create_table :tickets do |t|
       t.string :ticket_type, null: false
+      t.integer :event_id, null: false
 
       t.timestamps
     end
-    add_index :tickets :ticket_type
+    add_index :tickets, :event_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180205021733) do
+ActiveRecord::Schema.define(version: 20180205022855) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,14 @@ ActiveRecord::Schema.define(version: 20180205021733) do
     t.index ["location"], name: "index_events_on_location"
     t.index ["organizer_id"], name: "index_events_on_organizer_id"
     t.index ["title"], name: "index_events_on_title"
+  end
+
+  create_table "tickets", force: :cascade do |t|
+    t.string "ticket_type", null: false
+    t.integer "event_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_tickets_on_event_id"
   end
 
   create_table "users", force: :cascade do |t|
